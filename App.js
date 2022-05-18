5c9c8ab1a421e6777203322a729f0f6a579d810f
Ruchi Badkur <94377483+RuchiBadkur@users.noreply.github.com>
2022-05-18 14:29:52 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,23 @@
+function validate(){
+    var mail = document.getElementById("maill");
+    var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
+    var pass = document.getElementById("passs").value;
+  
+    if(mail=="" || !mail.value.match(validRegex)){
+      document.getElementsByTagName("small")[0].innerHTML = "Please enter a valid e-mail address";
+      return false;
+    }
+  
+   if(pass =="" ){
+      document.getElementsByTagName("small")[1].innerHTML = " You must enter password";
+      return false;
+   }
+   else{
+    document.getElementsByTagName("small")[0].innerHTML = "";
+    document.getElementsByTagName("small")[1].innerHTML = "";
+      
+   }
+
+
+}
